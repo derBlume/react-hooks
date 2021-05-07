33c1906af39bb25a3a8189cce1258b5a223031ca
Patrick Blume <71653434+derBlume@users.noreply.github.com>
2021-05-07 12:50:09 +0200

    all tasks finished

--- src/exercise/06.js
+++ src/exercise/06.js
@@ -2,30 +2,89 @@
 // http://localhost:3000/isolated/exercise/06.js
 
 import * as React from 'react'
-// 🐨 you'll want the following additional things from '../pokemon':
-// fetchPokemon: the function we call to get the pokemon info
-// PokemonInfoFallback: the thing we show while we're loading the pokemon info
-// PokemonDataView: the stuff we use to display the pokemon info
-import {PokemonForm} from '../pokemon'
+
+import {
+  fetchPokemon,
+  PokemonDataView,
+  PokemonForm,
+  PokemonInfoFallback,
+} from '../pokemon'
+
+import {ErrorBoundary} from 'react-error-boundary'
+
+/* class ErrorBoundary extends React.Component {
+  state = {error: null}
+  static getDerivedStateFromError(error) {
+    return {error}
+  }
+  render() {
+    const {error} = this.state
+    if (error) {
+      return <this.props.FallbackComponent error={error} />
+    }
+    return this.props.children
+  }
+} */
 
 function PokemonInfo({pokemonName}) {
-  // 🐨 Have state for the pokemon (null)
-  // 🐨 use React.useEffect where the callback should be called whenever the
-  // pokemon name changes.
-  // 💰 DON'T FORGET THE DEPENDENCIES ARRAY!
-  // 💰 if the pokemonName is falsy (an empty string) then don't bother making the request (exit early).
-  // 🐨 before calling `fetchPokemon`, clear the current pokemon state by setting it to null
-  // 💰 Use the `fetchPokemon` function to fetch a pokemon by its name:
-  //   fetchPokemon('Pikachu').then(
-  //     pokemonData => { /* update all the state here */},
-  //   )
-  // 🐨 return the following things based on the `pokemon` state and `pokemonName` prop:
-  //   1. no pokemonName: 'Submit a pokemon'
-  //   2. pokemonName but no pokemon: <PokemonInfoFallback name={pokemonName} />
-  //   3. pokemon: <PokemonDataView pokemon={pokemon} />
+  const [pokemon, setPokemon] = React.useState(null)
+  const [error, setError] = React.useState(null)
+  const [status, setStatus] = React.useState(pokemonName ? 'pending' : 'idle')
 
-  // 💣 remove this
-  return 'TODO'
+  React.useEffect(() => {
+    if (pokemonName === '') {
+      return
+    }
+
+    setStatus('pending')
+    //setPokemon(null)
+    //setError(null)
+
+    fetchPokemon(pokemonName)
+      .then(pokemonData => {
+        setPokemon(pokemonData)
+        setStatus('resolved')
+      })
+
+      .catch(error => {
+        setError(error)
+        setStatus('rejected')
+      })
+  }, [pokemonName])
+
+  if (status === 'rejected') throw error
+  else {
+    return (
+      <>
+        {status === 'pending' && <PokemonInfoFallback name={pokemonName} />}
+        {status === 'resolved' && <PokemonDataView pokemon={pokemon} />}
+        {status === 'idle' && 'Submit a pokemon'}
+
+        {/* {error ? (
+        <div role="alert">
+        There was an error:{' '}
+        <pre style={{whiteSpace: 'normal'}}>{error.message}</pre>
+        </div>
+        ) : pokemon ? (
+          <PokemonDataView pokemon={pokemon} />
+          ) : pokemonName ? (
+            <PokemonInfoFallback name={pokemonName} />
+            ) : (
+              'Submit a pokemon'
+            )} */}
+      </>
+    )
+  }
+}
+
+function ErrorFallback({error, resetErrorBoundary}) {
+  return (
+    <div role="alert">
+      There was an error:{' '}
+      <pre style={{whiteSpace: 'normal'}}>{error.message}</pre>
+      <button onClick={resetErrorBoundary}>Try again</button>
+    </div>
+  )
 }
 
 function App() {
@@ -40,7 +99,13 @@ function App() {
       <PokemonForm pokemonName={pokemonName} onSubmit={handleSubmit} />
       <hr />
       <div className="pokemon-info">
-        <PokemonInfo pokemonName={pokemonName} />
+        <ErrorBoundary
+          FallbackComponent={ErrorFallback}
+          onReset={() => setPokemonName('')}
+          resetKeys={[pokemonName]}
+        >
+          <PokemonInfo pokemonName={pokemonName} />
+        </ErrorBoundary>
       </div>
     </div>
   )
